c47ba567687e01bba952b76d1307418b3a1af7e4
YATHEESH NAGELLA <yatheeshnagella17@gmail.com>
2023-10-05 02:24:09 +0000

    Terraform Cloud Backend #11

--- main.tf
+++ main.tf
@@ -13,6 +13,12 @@ terraform {
     workspaces {
       name = "terra-house-1"
     }
+  }
+    required_providers {
+    random = {
+      source = "hashicorp/random"
+      version = "3.5.1"
+    }
     aws = {
       source = "hashicorp/aws"
       version = "5.16.2"
