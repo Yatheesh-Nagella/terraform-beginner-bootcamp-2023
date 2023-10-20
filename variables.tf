0aea89dfbba375eb63c798c69bd61347e557f27e
YATHEESH NAGELLA <yatheeshnagella17@gmail.com>
2023-10-20 04:45:18 +0000

    AWS Terrahouse module #23

--- variables.tf
+++ variables.tf
@@ -1,24 +1,7 @@
-
-# Generating uuid for the user
 variable "user_uuid" {
-  type        = string
-  description = "The UUID of the user"
-  validation {
-    condition     = can(regex("^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[1-5][0-9a-fA-F]{3}-[89aAbB][0-9a-fA-F]{3}-[0-9a-fA-F]{12}$", var.user_uuid))
-    error_message = "Invalid UUID format. Must be a valid UUID."
-  }
+ type = string
 }
 
 variable "bucket_name" {
-  description = "The name of the S3 bucket"
-  type        = string
-
-  validation {
-    condition     = (
-      length(var.bucket_name) >= 3 && length(var.bucket_name) <= 63 && 
-      can(regex("^[a-z0-9][a-z0-9-.]*[a-z0-9]$", var.bucket_name))
-    )
-    error_message = "The bucket name must be between 3 and 63 characters, start and end with a lowercase letter or number, and can contain only lowercase letters, numbers, hyphens, and dots."
-  }
+ type = string
 }
-
