c36988e27fd53a04e9e1dc24e484dee554911703
YATHEESH NAGELLA <yatheeshnagella17@gmail.com>
2023-11-02 17:54:00 +0000

    Assets Upload #34

--- main.tf
+++ main.tf
@@ -17,10 +17,11 @@ terraform {
 }
 
 module "terrahouse_aws" {
-  source = "./modules /terrahouse_aws"
+  source = "./modules/terrahouse_aws"
   user_uuid = var.user_uuid
   bucket_name = var.bucket_name
   index_html_filepath = var.index_html_filepath
   error_html_filepath = var.error_html_filepath
   content_version = var.content_version
+  assets_path = var.assets_path  # Add this line with the appropriate value
 }
